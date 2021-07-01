8d1a125e3f140e71b1697659fb4af7d6cf78fabe
yumandee <yumandee@yahoo.com>
2021-07-01 17:36:29 -0400

    AccountBalance implemented
    AccountBalance now reflects credits - debits

--- bank-of-react/src/components/AccountBalance.js
+++ bank-of-react/src/components/AccountBalance.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class AccountBalance extends Component {
    render() {
       return (
-         <div> Balance: {this.props.accountBalance} </div>
+         <div> Balance: ${this.props.accountBalance} </div>
       );
    }
 }
